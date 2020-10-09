69036e6d5bd55172af23db7458d1aba7bb2ea994
Catruya Lilian <liliancatruya@gmail.com>
2020-10-08 23:41:23 -0500

    creating posts backend is working so far

--- js/posts.js
+++ js/posts.js
@@ -4,7 +4,7 @@
  * @description contains logic for handling bloggo posts
  */
 
-import * as model from './models.js';
+const database = require('./database.js');
 
 
 /**
@@ -14,12 +14,44 @@ import * as model from './models.js';
  * 3) A backend database system
  */
 
+/**
+ * @function datePosted
+ * Returns the date in YYYY-MM-DD hh:mm:ss format
+ * This is used because MySQL requires that formatting
+ */
+
+function datePosted() {
+    const date = new Date(Date.now());
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+}
+
  /**
   * @function createPost 
   * Creates a Bloggo post - stores in database
   */
-export function createPost(user, title, text) {
+function createPost(user, title, text) {
     // Creates a new post object
-    let post = new model.Post(user, title, text, Date.now());
+    //let post = new model.Post(user, title, text, Date.now());
+    
+    // Insert post into POSTS in database
+    let sql = `
+        INSERT INTO POSTS (user, title, text, date_posted)
+        VALUES ('${user}', '${title}', '${text}', '${datePosted()}')
+    `;
+
+    database.query(sql);
+
+    console.log('Inserted post.');
+
+    database.end();
     
 }
+
+module.exports = {createPost};
